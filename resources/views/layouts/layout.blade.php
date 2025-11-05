903336f50705db64be503d82f27fe730e3d6a6f4
reymartin <reymartinagluya8@gmail.com>
2025-11-05 12:56:19 +0800

    ...

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', config('app.name', 'Laravel'))</title>   
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
     <div class="min-h-full">
